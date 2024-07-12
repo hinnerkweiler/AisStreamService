b5c25318cb8a1b2134ad2286da49ae654bfc8003
Hinnerk Weiler <post@hinnerk-weiler.de>
2024-07-12 15:46:50 +0200

    Database Migrations

---
+++ AisStreamService/Data/Migrations/20240712111025_fixuserid2.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AisStreamService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixuserid2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Mmsi",
+                table: "AIS_Vessels",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Mmsi",
+                table: "AIS_Vessels",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
---
+++ AisStreamService/Data/Migrations/20240712124835_addmetadataforships.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AisStreamService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addmetadataforships : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "AIS_Vessels",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Group",
+                table: "AIS_Vessels",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "AIS_Vessels",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShipUrl",
+                table: "AIS_Vessels",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "AIS_Vessels",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "AIS_Vessels");
+
+            migrationBuilder.DropColumn(
+                name: "Group",
+                table: "AIS_Vessels");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "AIS_Vessels");
+
+            migrationBuilder.DropColumn(
+                name: "ShipUrl",
+                table: "AIS_Vessels");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "AIS_Vessels");
+        }
+    }
+}
